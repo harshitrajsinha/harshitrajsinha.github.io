e8dd9937dcff34940544a3d7d33d97fb5c8912e3
Harshit <raj.harshitsinha08@gmail.com>
2026-09-09 18:18:55 +0530

    adding button tags for analytics tracking

--- src/script.js
+++ src/script.js
@@ -1,27 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const openBtn = document.getElementById("openVideo");
-  const modal = document.getElementById("videoModal");
-  const closeBtn = document.getElementById("closeVideo");
-  const video = document.getElementById("videoPlayer");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const openBtn = document.getElementById("openVideo");
+//   const modal = document.getElementById("videoModal");
+//   const closeBtn = document.getElementById("closeVideo");
+//   const video = document.getElementById("videoPlayer");
 
-  // Close Modal on Click Outside Video
-  window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-      video.pause();
-    }
-  });
+//   // Close Modal on Click Outside Video
+//   window.addEventListener("click", (e) => {
+//     if (e.target === modal) {
+//       modal.style.display = "none";
+//       video.pause();
+//     }
+//   });
 
-  // Open Modal & Play Video
-  openBtn.addEventListener("click", () => {
-    modal.style.display = "flex";
-    video.currentTime = 0;
-    video.play();
-  });
+//   // Open Modal & Play Video
+//   openBtn.addEventListener("click", () => {
+//     modal.style.display = "flex";
+//     video.currentTime = 0;
+//     video.play();
+//   });
 
-  // Close Modal & Pause Video
-  closeBtn.addEventListener("click", () => {
-    video.pause();
-    modal.style.display = "none";
+//   // Close Modal & Pause Video
+//   closeBtn.addEventListener("click", () => {
+//     video.pause();
+//     modal.style.display = "none";
+//   });
+// });
+
+// Register button click on Google Analytics
+
+function trackButtonClick(buttonName) {
+  gtag('event', 'button_click', {
+    'button_name': buttonName
   });
-});
+}
